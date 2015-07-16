2a8dc840eb94084d19305936dad1a1058cff132f
Bryce Davis <me@bryceadavis.com>
2015-07-16 14:43:37 -0400

    Added init spec for pretty json configs

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 describe "Init" do
+    before :all do
+        @json = '{"files":["index.html",".markwiki.cfg"],"css":{"dir_name":"css","files":["styles.css"]},"js":{"dir_name":"js","files":["scripts.js"]},"img":{"dir_name":"img","files":[]}}'
+        @pjson = "{\n  \"files\": [\n    \"index.html\",\n    \".markwiki.cfg\"\n  ],\n  \"css\": {\n    \"dir_name\": \"css\",\n    \"files\": [\n      \"styles.css\"\n    ]\n  },\n  \"js\": {\n    \"dir_name\": \"js\",\n    \"files\": [\n      \"scripts.js\"\n    ]\n  },\n  \"img\": {\n    \"dir_name\": \"img\",\n    \"files\": [\n\n    ]\n  }\n}"
+    end
+    
     it "can generate a json configuration" do
-        json = '{"files":["index.html",".markwiki.cfg"],"css":{"dir_name":"css","files":["styles.css"]},"js":{"dir_name":"js","files":["scripts.js"]},"img":{"dir_name":"img","files":[]}}'
-        expect(Markwiki::Init.generate_json_config).to eq(json)
+        expect(Markwiki::Init.generate_json_config).to eq(@json)
+    end
+
+    it "can generate a pretty json configuration" do
+        expect(Markwiki::Init.generate_pretty_json_config).to eq(@pjson)
     end
 end
