855c39901bda057ad07b0e16efd3048b5f2012ae
Bryce Davis <me@bryceadavis.com>
2015-07-16 14:55:02 -0400

    Begin documentation

--- lib/markwiki/init.rb
+++ lib/markwiki/init.rb
@@ -1,3 +1,4 @@
+# @author Bryce Davis <me@bryceadavis.com>
 require 'json' # For JSON parsing
 
 module Markwiki
@@ -18,14 +19,23 @@ module Markwiki
     }
 
     class Markwiki::Init
+        # @todo Finish site initialization
         def self.init_site(site_name)
             
         end
 
+        # Generate a JSON String representation of a Markwiki
+        # site.
+        #
+        # @return [String] the Markwiki skeleton as a JSON String
         def self.generate_json_config
             Markwiki::CONFIG.to_json
         end
 
+        # Generate a prettified JSON String representation of a
+        # Markwiki site.
+        #
+        # @return [String] the Markwiki skeleton as a JSON String
         def self.generate_pretty_json_config
             JSON.pretty_generate Markwiki::CONFIG
         end
