d5520d59287eeddcd9e210c01c7665f84a13cc37
Bryce Davis <me@bryceadavis.com>
2015-07-17 13:42:47 +0000

    Update tests for Coveralls coverage

--- spec/markwiki_spec.rb
+++ spec/markwiki_spec.rb
@@ -1,11 +1,34 @@
 require 'spec_helper'
 
 describe Markwiki do
+  before :all do
+    @name = "markwiki"
+  end
+
   it 'has a version number' do
     expect(Markwiki::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(true).to eq(true)
+  describe 'creating a new Markwiki site' do
+    before :all do
+      Markwiki::Init.init_site(@name)
+    end
+
+    after :all do
+      FileUtils.rm_rf(@name)
+    end
+    
+    it 'creates the top-level directory' do
+      top = Dir.exists? @name
+      expect(top).to eq(true)
+    end
+
+    it 'creates the index page' do
+      expect(File.exists? "#{@name}/index.html").to eq(true)
+    end
+
+    it 'creates the Markwiki configuration file' do
+      expect(File.exists? "#{@name}/.markwiki.cfg").to eq(true)
+    end
   end
 end
