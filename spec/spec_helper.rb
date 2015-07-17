1ee04cb2c67d93a136eea5a24c72fd1ac8339085
Bryce Davis <me@bryceadavis.com>
2015-07-17 13:22:49 +0000

    Add Coveralls.wear! call to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'coveralls' # For Coveralls.io service
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'markwiki'
 require 'fileutils' # For deleting directories
